52d4f3eb7e2ee89ebdf1bab8f58ecfbd3d596411
George Júnior <127257970+NoxusJr@users.noreply.github.com>
2023-11-17 23:00:54 -0300

    Update andar.py

--- package/andar.py
+++ package/andar.py
@@ -25,7 +25,7 @@ def press_key(key, command):
 def frente(maoDireita_x,cotoveloDireito_x,dist_maoDireita_cintura,dist_entreMaos, altura_maoDireita, altura_cintura):
     global key_right_pressed
 
-    if (dist_maoDireita_cintura > 90) and (dist_entreMaos > 100 ) and (altura_maoDireita < (altura_cintura-30)):
+    if (dist_maoDireita_cintura > 60) and (dist_entreMaos > 100 ) and (altura_maoDireita < (altura_cintura-30)):
         
         if key_right_pressed is False:
             press_key_in_background('right', 'keydown')
@@ -48,7 +48,7 @@ def frente(maoDireita_x,cotoveloDireito_x,dist_maoDireita_cintura,dist_entreMaos
 def tras(maoEsquerda_x,cotoveloEsquerdo_x,dist_maoEsquerda_cintura,dist_entreMaos,altura_maoEsquerda,altura_cintura):
     global key_left_pressed
     
-    if (dist_maoEsquerda_cintura > 90) and (dist_entreMaos > 100) and (altura_maoEsquerda < (altura_cintura-30)):
+    if (dist_maoEsquerda_cintura > 60) and (dist_entreMaos > 100) and (altura_maoEsquerda < (altura_cintura-30)):
 
         if key_left_pressed is False:
             press_key_in_background('left', 'keydown')
